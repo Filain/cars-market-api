1fe427c16129e687eb4a9708e0025b0ce27db3fa
Filain <ratecurrent@gmail.com>
2024-03-09 10:24:50 +0200

    add eslint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { SwaggerHelper } from './common/helpers/swagger.helper';
+import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Cars market API')
+    .setDescription('Short description of CARS API')
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'header',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerHelper.setDefaultResponses(document);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      docExpansion: 'list',
+      defaultModelExpandDepth: 3,
+      persistAuthorization: true,
+    },
+  });
   await app.listen(3000);
 }
 bootstrap();
